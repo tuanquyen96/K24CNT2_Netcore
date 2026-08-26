f60a2863e8dab6839b0b69dccae7839e03d58723
VTQuyen <radahotga1@gmail.com>
2026-08-26 14:57:39 +0700

    Upload bai

---
+++ buoi3/buoi3/Controllers/VTQproductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using buoi3.Models;
+
+namespace Lesson_2.Controllers
+{
+    public class VTQproductController : Controller
+    {
+        public IActionResult VTQIndex()
+        {
+            ViewBag.name = "Vũ Tuấn Quyền";
+            ViewData["productVD"] = "Iphone 14 Pro Max";
+            TempData["UNI"] = "Đại học Nguyễn Trãi - NTU";
+
+            return View();
+        }
+
+        public IActionResult GetProduct()
+        {
+            var product = new VTQProduct
+            {
+                ProductID = "2410900066",
+                ProductName = "Iphone 14 Pro Max",
+                YearRelease = 2006,
+                Price = 30000000
+            };
+
+            ViewBag.product = product;
+            ViewBag.ProductID = product.ProductID;
+            ViewBag.ProductName = product.ProductName;
+            ViewBag.YearRelease = product.YearRelease;
+            ViewBag.Price = product.Price;
+            ViewData["product"] = product;
+
+            return View("Product");
+        }
+    }
+}
---
+++ buoi3/buoi3/Models/VTQProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace buoi3.Models
+{
+    public class VTQProduct
+    {
+        public string ProductID { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public int YearRelease { get; set; }
+        public decimal Price { get; set; }
+    }
+}
