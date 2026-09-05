c9d831ef53fbaf225c9afb2d58ae877a5a740f2d
VTQuyen <radahotga1@gmail.com>
2026-09-05 11:43:05 +0700

    Upload Lesson5

---
+++ 5-9-2026/Lesson5/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using _5_9_2026.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _5_9_2026.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult RazorCode()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 5-9-2026/Lesson5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _5_9_2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
